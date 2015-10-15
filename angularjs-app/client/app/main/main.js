304bcf07428152b39cfcbf74012e5a8fbd3c2d58
Jose Baena <jose@brandwatch.com>
2015-10-14 17:26:40 -0700

    Added angular js base setup - Used from angular full stack generator

---
+++ angularjs-app/client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angularjsAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
